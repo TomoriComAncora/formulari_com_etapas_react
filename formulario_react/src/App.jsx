f6800c5bd6ff66a1f2367a4f0905d3e487f1a679
TomoriComAncora <lucas_fernando@ufms.br>
2023-12-15 09:41:23 -0400

    FEAT: Criando a lógica e estilo da progressão dos passos

--- formulario_react/src/App.jsx
+++ formulario_react/src/App.jsx
@@ -4,6 +4,7 @@ import { FiSend } from "react-icons/fi";
 import UserForm from "./components/UserForm";
 import ReviewForm from "./components/ReviewForm";
 import Thanks from "./components/Thanks";
+import Steps from "./components/Steps";
 
 // hooks
 import { useForm } from "./hooks/useForm";
@@ -26,7 +27,7 @@ function App() {
         </p>
       </div>
       <div className="form-container">
-        <p>Etapas</p>
+        <Steps currentStep={step}/>
         <form onSubmit={(e) => changeStep(step + 1, e)}>
           <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
