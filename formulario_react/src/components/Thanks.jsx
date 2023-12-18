a7a5306131f26a33c38db0c621e43d45825324d9
TomoriComAncora <lucas_fernando@ufms.br>
2023-12-18 12:13:21 -0400

    FEAT: Iniciando o componente

--- formulario_react/src/components/Thanks.jsx
+++ formulario_react/src/components/Thanks.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from "react-icons/bs";
 import "./Thanks.css"
 
 function Thanks() {
   return (
-    <div>
-        <h2>Thanks</h2>
+    <div className="thanks-container">
+      <h2>Falta pouco...</h2>
+      <p>
+        A sua opnião é muito importante, em breve você receberá um cupom de 10% de desconto para a sua próxima compra.
+      </p>
+      <p>
+        Para concluir sua avaliação clique no botão de Enviar abaixo
+      </p>
+      <h3>Aqui está o resumo da sua avaliação:</h3>
+      <p className="review-data">
+        <span>Satisfação com o produto: {<BsFillEmojiHeartEyesFill/>}</span>
+      </p>
+      <p className="review-data">
+        <span>Comentário:</span>
+      </p>
     </div>
   )
 }
